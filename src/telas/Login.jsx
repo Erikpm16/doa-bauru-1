b57d9b92a22e51a5e1b8b009110919f1a644ab3e
Luiz Felipe Porto <45501206+fehporto97@users.noreply.github.com>
2022-06-22 17:28:27 -0300

    Update Login.jsx

--- src/telas/Login.jsx
+++ src/telas/Login.jsx
@@ -1,27 +1,28 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import icon from '../telas/img/icon-login.png';
+
+import './styles/Login.css'
 
 export default function Login() {
   return (
-    <Form className='container' >
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
+    <div className='Tela-login'>
+      <Form>
+        <Form.Group>
+        <img src={icon} className="img" />
+        <br/>
+        <p>Fazer Login</p>
+          <Form.Control className='input' type="email" placeholder="Email" />
+          <br/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control type="password" placeholder="Senha" />
+        </Form.Group>
+        <Button  className='btn-login' variant="primary" type="submit">
+          Entrar
+        </Button>
+        <a href='#' className='criar-conta' >Criar sua conta</a>
+      </Form>
+    </div>
+  )
 }
